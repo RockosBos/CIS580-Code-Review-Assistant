f2c85ba790d53e36dde87bb50fee551c20259226
Meaghan Bryant <meg.marie.bryant@gmail.com>
2025-10-19 03:55:08 -0400

    Set up initial Flask templates. Set up Bootstrap app. Insert Bootstrap cover page template.

---
+++ main.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap5
+
+#initialize Flask App
+app = Flask(__name__)
+
+#initializ Bootstrap
+Bootstrap5(app)
+
+#TODO Home Route
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+#TODO Results page
+@app.route("/results")
+def show_results():
+    pass
+
+
+if __name__ == "__main__":
+    app.run(debug = True, port = 8080)
