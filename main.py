9bff2d73056a4c9271304f45a9a46ba2e3475612
megbry <megbry@umich.edu>
2025-10-21 00:44:30 -0400

    Add show_results.html. Add request.form.get to retrieve repository URL from user.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_bootstrap import Bootstrap5
 import pydriller
 import os
@@ -18,15 +18,21 @@ def home():
     return render_template('index.html')
 
 #TODO Selection Page
-@app.route("/select")
+@app.route('/select')
 def select_repo():
     return render_template('select_repo.html')
 
 #TODO Results page
-@app.route('/results')
+@app.route('/results', methods = ['GET', 'POST'])
 def show_results():
-    pass
+    repository_url = None
+    # print(request.method)
+    if request.method == 'POST':
+        repository_url = request.form.get('repository_url')
+        print(repository_url)
+    return render_template('show_results.html', repository_url = repository_url)
 
+# 'C:/Users/Owner/PycharmProjects/CIS580-Code-Review-Assistant'
 
 if __name__ == '__main__':
     app.run(debug = True, port = 8080)
