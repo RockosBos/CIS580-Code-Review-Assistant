e8e589979cd009926428089034a740db5f5519db
RockosBos <nkessey9817@gmail.com>
2025-11-26 17:01:13 -0500

    Added recent files and a return to repo select button

--- Results.py
+++ Results.py
@@ -2,13 +2,18 @@ class Results:
 
 	fileName = ""
 	bugDensity = 0.0
+	lastEdit = ""
 
-	def __init__(self, file, density):
+	def __init__(self, file, density, recentCommitDate):
 		self.fileName = file
 		self.bugDensity = density
+		self.lastEdit = recentCommitDate
 
 	def getFile(self):
 		return str(self.fileName)
 	
 	def getDensity(self):
 		return str(self.bugDensity)
+	
+	def getLastCommitDate(self):
+		return str(self.lastEdit)
--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from statisical_interface import StatisticsInterface
 from Results import Results
 
 import csv
-import requests
+import operator
 
 #safety measure to minimize PyDriller errors during processing
 os.environ['GIT_LFS_SKIP_SMUDGE'] = '1'
@@ -34,52 +34,70 @@ llm_interface = LLMInterface()
 statistical_interface = StatisticsInterface()
 
 resultList = list()
+densityList = list()
+TSList = list()
+filteredDensityList = list()
+filteredTSList = list()
 
 #TODO Home Route
 @app.route('/')
 def home():
-    return render_template('index.html')
+	return render_template('index.html')
 
 #TODO Selection Page
 @app.route('/select', methods = ['GET', 'POST'])
 def select_repo():
-    if request.method == 'POST':
-        # TODO - ideally validate REPO HERE if time permits
-        repo_url = request.form.get('repository_url')
-        print(f' Select_repo: {repo_url}')
-        if repo_url is None:
-            flash('The selected repository is not accessible. Please try again with another repository.')
-            return redirect(url_for('select_repo'))
-        else:
-            print('It worked!')
-            
-            commits = repository_interface.retrieve_repository(repo_url)
-            classification_results = llm_interface.process_commits(commits)
-            statistical_results = statistical_interface.analyze_results(classification_results)
-            
-            resultList.clear()
-            for i in statistical_results.itertuples(index=False):
-                result = Results(i.filename, i.bug_density)
-                resultList.append(result)
-                
-            return redirect(url_for('show_results', repository_url=repo_url))
-    return render_template('select_repo.html')
+	if request.method == 'POST':
+		# TODO - ideally validate REPO HERE if time permits
+		repo_url = request.form.get('repository_url')
+		print(f' Select_repo: {repo_url}')
+		if repo_url is None:
+			flash('The selected repository is not accessible. Please try again with another repository.')
+			return redirect(url_for('select_repo'))
+		else:
+			print('It worked!')
+			
+			commits = repository_interface.retrieve_repository(repo_url)
+			classification_results = llm_interface.process_commits(commits)
+			statistical_results = statistical_interface.analyze_results(classification_results)
+			
+			resultList.clear()
+			for i in statistical_results.itertuples(index=False):
+				result = Results(i.filename, i.bug_density, i.last_commit_date)
+				resultList.append(result)
+
+			return redirect(url_for('show_results', repository_url=repo_url))
+	return render_template('select_repo.html')
 
 #TODO Results page
 #TODO page should include info from statistics
 @app.route('/results', methods = ['GET', 'POST'])
 def show_results():
-    if request.method == "POST":
-        if 'genCSVButton' in request.form:
-            with open('results.csv', 'w', newline='') as f:
-                fieldNames = ["File", "Bug Density"]
-                writer = csv.DictWriter(f, fieldnames=fieldNames)
-                writer.writeheader()
-                for i in resultList:
-                    writer.writerow({"File": i.getFile(), "Bug Density": i.getDensity()})
-            return render_template('show_results.html', resultData=resultList)
-    else:        
-    	return render_template('show_results.html', resultData=resultList)
+	if request.method == "POST":
+		if 'genCSVButton' in request.form:
+			with open('results.csv', 'w', newline='') as f:
+				fieldNames = ["File", "Bug Density", "Last Commit Date"]
+				writer = csv.DictWriter(f, fieldnames=fieldNames)
+				writer.writeheader()
+				for i in resultList:
+					writer.writerow({"File": i.getFile(), "Bug Density": i.getDensity(), "Last Commit Date":i.getLastCommitDate()})
+			return render_template('show_results.html', resultData=resultList, tsData=TSList)
+		if 'ReturnToRepoSelectButton' in request.form:
+			return render_template('select_repo.html')
+	else:        
+		TSList = sorted(resultList, key=operator.attrgetter('lastEdit'), reverse=True)
+		filteredTSList = [f for f in TSList if (f.getDensity() != "0.0")]
+		TSList.clear()
+		iter = 0
+		for i in filteredTSList:
+			if(iter < 5):
+				TSList.append(i)
+				iter = iter + 1
+			else:
+				break
+	for i in TSList:
+		print(i.getFile(), i.getDensity())
+	return render_template('show_results.html', resultData=resultList, tsData=TSList)
 
 if __name__ == '__main__':
-    app.run(debug = True, port = 8080)
+	app.run(debug = True, port = 8080)
