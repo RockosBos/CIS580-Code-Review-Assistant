a8912dc00a0560858e6a0234ad8638bcda05c3a9
megbry <megbry@umich.edu>
2025-11-27 19:21:26 -0500

    Change masking to keep file extensions vs dropping, files are being correctly flagged as 0 when not on the keep list.

--- statisical_interface.py
+++ statisical_interface.py
@@ -1,5 +1,6 @@
 import pandas as pd
 
+
 class StatisticsInterface:
     def __init__(self):
         pass
@@ -7,28 +8,32 @@ class StatisticsInterface:
     def analyze_results(self, results):
         df = pd.DataFrame(results)
 
-        #expand the modified files field to
-        df = df.explode('modified_files', ignore_index = True)
+        # expand the modified files field to
+        df = df.explode('modified_files', ignore_index=True)
 
-        #convert modfiles into files, deletions, insertions at file level
+        # convert modfiles into files, deletions, insertions at file level
         df['filename'] = df['modified_files'].apply(lambda d: d['path'] if isinstance(d, dict) else None)
         df['file_added'] = df['modified_files'].apply(lambda d: d['added_lines'] if isinstance(d, dict) else 0)
         df['file_deleted'] = df['modified_files'].apply(lambda d: d['deleted_lines'] if isinstance(d, dict) else 0)
 
-        df = df.drop(columns = ['modified_files'])
+        df = df.drop(columns=['modified_files'])
 
         df['classification'] = df['classification'].astype(str).str.strip().str.lower()
         df['is_bug_fix'] = df['classification'].eq('bug fix').astype(int)
         df['is_not_labeled'] = df['classification'].eq('not labeled').astype(int)
 
-        # non-code files are being flagged as bug fixes due to the commit messages - exclude these files by setting is_bug_fix = 0
-        drop_extensions = ('.gitignore', '.suo', '.backup', '.png', '.jpg', '.jpeg', '.meta', '.settings',
-                           '.prefab', '.unity', '.wav', '.asset', '.fbx', '.bin', '.zip', '.pak',
-                           '.dll', '.exe', '.tiff', '.tif', '.mp4', '.mp3', '.mov', '.info')
+        # help ensure filename matching by formatting the names
+        filename_series = df['filename'].fillna('').astype(str).str.strip().str.lower()
 
-        filename_series = df['filename'].astype(str).str.lower()
-        drop_mask = filename_series.str.endswith(drop_extensions, na = False)
-        df.loc[drop_mask, 'is_bug_fix'] = 0
+        # isolate file extensions to assist with matching
+        df['ext'] = filename_series.str.extract(r'(\.[^.\\]+(?:\.[^.\\]+)*)$', expand=False)
+
+        keep_extensions = ('.py', '.java', '.class', '.jar', '.c', '.cpp', '.h', '.hpp', '.hxx', '.html', '.htm',
+                           '.css', '.js', '.php', '.cs', '.swift', '.vb', '.sh', '.pl', '.go', '.rb')
+
+        keep_mask = df['ext'].isin(keep_extensions)
+        df.loc[~keep_mask, 'is_bug_fix'] = 0
+
 
         aggregate_result = df.groupby('filename', as_index=False).agg({
             'hash': 'count',
@@ -47,12 +52,13 @@ class StatisticsInterface:
 
         aggregate_result['bug_density'] = (aggregate_result['bug_fix_commits'] / aggregate_result['commit_count'])
 
-        #health metric to monitor how many LLM classifications were unsuccessful
-        aggregate_result['failed_classification_density'] = (aggregate_result['is_not_labeled'] / aggregate_result['commit_count'])
+        # health metric to monitor how many LLM classifications were unsuccessful
+        aggregate_result['failed_classification_density'] = (
+                    aggregate_result['is_not_labeled'] / aggregate_result['commit_count'])
 
-        aggregate_result = aggregate_result.sort_values('bug_density', ascending = False)
+        aggregate_result = aggregate_result.sort_values('bug_density', ascending=False)
         aggregate_result = aggregate_result.reset_index()
 
-        aggregate_result.to_csv('output.csv', index = False)
+        aggregate_result.to_csv('output.csv', index=False)
 
         return aggregate_result
