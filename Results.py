c234e97974c408bf4e9e2ab499e1ffa0bcabcc15
megbry <megbry@umich.edu>
2025-12-01 01:00:30 -0500

    Update naming to be consistent across all code files.

--- Results.py
+++ Results.py
@@ -1,19 +1,19 @@
 class Results:
 
-	fileName = ""
-	bugDensity = 0.0
-	lastEdit = ""
+	file_name = ""
+	bug_density = 0.0
+	last_edit = ""
 
-	def __init__(self, file, density, recentCommitDate):
-		self.fileName = file
-		self.bugDensity = density
-		self.lastEdit = recentCommitDate
+	def __init__(self, file, density, recent_commit_date):
+		self.file_name = file
+		self.bug_density = density
+		self.last_edit = recent_commit_date
 
-	def getFile(self):
+	def get_file(self):
 		return str(self.fileName)
 	
-	def getDensity(self):
+	def get_density(self):
 		return str(self.bugDensity)
 	
-	def getLastCommitDate(self):
+	def get_last_commit_date(self):
 		return str(self.lastEdit)
